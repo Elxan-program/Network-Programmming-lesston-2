d7c8ab5c6d705f4280cf777588b1a41b4e80b367
Atay_qn28 <Atay_qn28@STHQ0118-08>
2021-12-21 12:00:10 +0400

    Add project files.

---
+++ Network Programmming lesston 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_Programmming_lesston_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var ip = IPAddress.Parse("10.1.18.38");
+			var port = 27001;
+			using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+			{
+				var ep = new IPEndPoint(ip, port);
+				socket.Bind(ep);
+
+				socket.Listen(10);
+				Console.WriteLine($"Listening on {socket.LocalEndPoint}");
+
+				while (true)
+				{
+					var client = socket.Accept();
+					Task.Run(() => { 
+					Console.WriteLine($"{client.RemoteEndPoint} Connected . . . .");
+
+					var length = 0;
+					var bytes = new byte[1024];
+
+					do
+					{
+						length = client.Receive(bytes);
+						var msg = Encoding.UTF8.GetString(bytes, 0, length);
+						Console.WriteLine($"Client : {client.RemoteEndPoint} : {msg}");
+							if (msg=="ok")
+							{
+								client.Shutdown(SocketShutdown.Both);
+								client.Dispose();
+								break;
+							}
+					} while (true);
+					});
+				}
+			}
+		}
+	}
+}
---
+++ TCP Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Client
+{
+	class Program
+	{
+		static void Send()
+		{
+			var soket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			var ip = IPAddress.Parse("10.1.18.38");
+			var port = 27001;
+			var ep = new IPEndPoint(ip, port);
+			try
+			{
+				soket.Connect(ep);
+				if (soket.Connected)
+				{
+					Console.WriteLine("Connected to the server . . . . .");
+					while (true)
+					{
+						var msg = Console.ReadLine();
+						var bytes = Encoding.UTF8.GetBytes(msg);
+						soket.Send(bytes);
+					}
+				}
+				else
+				{
+					Console.WriteLine("Can not connected to the server . . . . .");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Can not connected to the server . . . . .");
+				Console.WriteLine(ex.Message);
+			}
+		}
+		static void Recieve()
+		{
+
+		} 
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Task.Run(() => {
+					Send();
+				});
+			}
+		}
+	}
+}
